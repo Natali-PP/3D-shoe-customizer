39903a19ede6117ddb86fc1292d84775f35028b2
Natali Palacio Pastor <cristinanatalip@gmail.com>
2023-02-22 17:31:38 -0300

    Added responsive behavior to the model and scene

--- src/components/scene/Scene.jsx
+++ src/components/scene/Scene.jsx
@@ -3,33 +3,22 @@ import { useLoader } from "@react-three/fiber";
 import { Environment, OrbitControls, useGLTF} from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Suspense, useRef, useContext } from "react";
-import ModelShoe from './models/ModelShoe.jsx'
-import { useControls } from 'leva'
-import {CustomizationContext} from '../../context/CustomizationContex.jsx';
+import ModelShoe from "./models/ModelShoe.jsx";
+import { useControls } from "leva";
+import { CustomizationContext } from "../../context/CustomizationContex.jsx";
 
-function Scene(){
-
-  const {onOpenModal} = useContext( CustomizationContext );
-  return(
-
-    <Canvas camera={{position:[0,0.5,1.05]}}>
+function Scene() {
+  const { onOpenModal } = useContext(CustomizationContext);
+  return (
+    <Canvas camera={{ position: [0, 0.5, 1.05] }}>
       <Suspense fallback={<p>Loading...</p>} />
-      {/*<axesHelper />
-        <gridHelper />*/}
-        <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
-        <directionalLight intensity={0.1} position={[100,0,50]} />
-        <ModelShoe />
-        <Environment 
-          //preset="night" 
-          //files="./img/syntwave-2.hdr" 
-          files="./img/xoor.hdr" 
-          blur={0.41}
-          //blur={0.05}
-          background />
+      <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
+      <directionalLight intensity={0.1} position={[100, 0, 50]} />
+      <ModelShoe />
+      <Environment files="./img/xoor.hdr" blur={0.41} background />
       <Suspense />
     </Canvas>
-  )
-
+  );
 }
 
 export default Scene;
--- src/components/scene/models/ModelShoe.jsx
+++ src/components/scene/models/ModelShoe.jsx
@@ -1,38 +1,91 @@
-import { useGLTF, GradientTexture } from "@react-three/drei";
+import { useGLTF, Bounds } from "@react-three/drei";
 import { useRef, useContext, useEffect } from "react";
-import {CustomizationContext} from '../../../context/CustomizationContex.jsx';
-import { useThree } from '@react-three/fiber'
+import { CustomizationContext } from "../../../context/CustomizationContex.jsx";
+import { useThree } from "@react-three/fiber";
 
-function ModelShoe({...props}){
-  const { nodes, materials } = useGLTF('/model.glb');
+function ModelShoe({ ...props }) {
+  const { nodes, materials } = useGLTF("/model.glb");
   const group = useRef();
-  const { setLayerColor,  setIsOpenModal, setLayerName, layerColor, innerColor, customization, setCustomization } = useContext( CustomizationContext );
+  const { setLayerColor, setIsOpenModal, setLayerName, layerColor, innerColor, customization, setCustomization } =
+    useContext(CustomizationContext);
+  const { viewport } = useThree();
 
   const handleClick = (e) => {
     e.stopPropagation();
-    setCustomization( prevState => ({
+    setCustomization((prevState) => ({
       ...prevState,
-      layerName: e.object.material.name
+      layerName: e.object.material.name,
     }));
     setIsOpenModal(true);
-  }
-
+  };
 
   return (
-    <group {...props} ref={group} dispose={null} >
-      <mesh geometry={nodes.shoe.geometry} material={materials.laces}  material-color={customization.layerColor.laces} onClick={(e) => handleClick(e)} scale={[customization.layerSize.laces.x,customization.layerSize.laces.y,customization.layerSize.laces.z]} />
-      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} material-color={customization.layerColor.mesh} 
-        scale={[customization.layerSize.mesh.x,customization.layerSize.mesh.y,customization.layerSize.mesh.z]} 
-        onClick={(e) => handleClick(e)} />
+    <Bounds fit clip observe margin={0.8}>
+      <group {...props} ref={group} dispose={null}>
+        <mesh
+          geometry={nodes.shoe.geometry}
+          material={materials.laces}
+          material-color={customization.layerColor.laces}
+          onClick={(e) => handleClick(e)}
+          scale={[customization.layerSize.laces.x, customization.layerSize.laces.y, customization.layerSize.laces.z]}
+        />
+        <mesh
+          geometry={nodes.shoe_1.geometry}
+          material={materials.mesh}
+          material-color={customization.layerColor.mesh}
+          scale={[customization.layerSize.mesh.x, customization.layerSize.mesh.y, customization.layerSize.mesh.z]}
+          onClick={(e) => handleClick(e)}
+        />
 
-      <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} material-color={customization.layerColor.caps} onClick={(e) => handleClick(e)} scale={[customization.layerSize.caps.x,customization.layerSize.caps.y,customization.layerSize.caps.z]} />
-      <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} material-color={customization.layerColor.inner} onClick={(e) => handleClick(e)} scale={[customization.layerSize.inner.x,customization.layerSize.inner.y,customization.layerSize.inner.z]} />
-      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} material-color={customization.layerColor.sole} onClick={(e) => handleClick(e)} scale={[customization.layerSize.sole.x,customization.layerSize.sole.y,customization.layerSize.sole.z]} />
-      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} onClick={(e) => handleClick(e)} material-color={customization.layerColor.stripes} scale={[customization.layerSize.stripes.x,customization.layerSize.stripes.y,customization.layerSize.stripes.z]} />
-      <mesh geometry={nodes.shoe_6.geometry} material={materials.band} onClick={(e) => handleClick(e)} material-color={customization.layerColor.band}  scale={[customization.layerSize.band.x,customization.layerSize.band.y,customization.layerSize.band.z]} />
-      <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} onClick={(e) => handleClick(e)} material-color={customization.layerColor.patch}  scale={[customization.layerSize.patch.x,customization.layerSize.patch.y,customization.layerSize.patch.z]} />
-    </group>
-  )
+        <mesh
+          geometry={nodes.shoe_2.geometry}
+          material={materials.caps}
+          material-color={customization.layerColor.caps}
+          onClick={(e) => handleClick(e)}
+          scale={[customization.layerSize.caps.x, customization.layerSize.caps.y, customization.layerSize.caps.z]}
+        />
+        <mesh
+          geometry={nodes.shoe_3.geometry}
+          material={materials.inner}
+          material-color={customization.layerColor.inner}
+          onClick={(e) => handleClick(e)}
+          scale={[customization.layerSize.inner.x, customization.layerSize.inner.y, customization.layerSize.inner.z]}
+        />
+        <mesh
+          geometry={nodes.shoe_4.geometry}
+          material={materials.sole}
+          material-color={customization.layerColor.sole}
+          onClick={(e) => handleClick(e)}
+          scale={[customization.layerSize.sole.x, customization.layerSize.sole.y, customization.layerSize.sole.z]}
+        />
+        <mesh
+          geometry={nodes.shoe_5.geometry}
+          material={materials.stripes}
+          onClick={(e) => handleClick(e)}
+          material-color={customization.layerColor.stripes}
+          scale={[
+            customization.layerSize.stripes.x,
+            customization.layerSize.stripes.y,
+            customization.layerSize.stripes.z,
+          ]}
+        />
+        <mesh
+          geometry={nodes.shoe_6.geometry}
+          material={materials.band}
+          onClick={(e) => handleClick(e)}
+          material-color={customization.layerColor.band}
+          scale={[customization.layerSize.band.x, customization.layerSize.band.y, customization.layerSize.band.z]}
+        />
+        <mesh
+          geometry={nodes.shoe_7.geometry}
+          material={materials.patch}
+          onClick={(e) => handleClick(e)}
+          material-color={customization.layerColor.patch}
+          scale={[customization.layerSize.patch.x, customization.layerSize.patch.y, customization.layerSize.patch.z]}
+        />
+      </group>
+    </Bounds>
+  );
 }
 
-export default ModelShoe
+export default ModelShoe;
